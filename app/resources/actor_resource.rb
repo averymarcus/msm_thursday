e4588039ee4fa4534e385077446b5e89277d29c0
anonymous anonymous <avery.marcus@kellogg.northwestern.edu>
2022-02-22 00:16:13 +0000

    Run rubocop

--- app/resources/actor_resource.rb
+++ app/resources/actor_resource.rb
@@ -6,11 +6,10 @@ class ActorResource < ApplicationResource
 
   # Direct associations
 
-  has_many   :roles
+  has_many :roles
 
   # Indirect associations
 
   many_to_many :filmography,
                resource: MovieResource
-
 end
